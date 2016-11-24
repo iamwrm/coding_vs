20941c1d6907e852b1208cf815440f5ace1b1d5b
Ren Wang <iamwrm@gmail.com>
2016-11-24 19:05:22 +0800

    尝试了链表初始化，然而内存貌似出错了

--- 链表/链表/源.c
+++ 链表/链表/源.c
@@ -3,12 +3,38 @@
 #include<math.h>
 #include<string.h>
 
-typedef struct
 
-int main() 
+typedef struct node_type
 {
+	double data;
+	struct node_type *prev;
+	struct node_type *next;
+}node;
+
+node* initial_list(double value)
+{
+	node* head;
+	head = (node*)malloc(sizeof(node*));
+	head->data = value;
+	head->prev = NULL;
+	head->next = NULL;
 	
+	return head;
+}
+
+
+
+
+int main()
+{
+	node* a;
+	a = initial_list(5.0);
+	getchar();
+
+	//	system("pause");
+	return 0;
+
+
 
 
 }
-
