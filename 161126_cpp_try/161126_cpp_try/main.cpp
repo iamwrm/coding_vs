faed519b1b9d2baa916c5252556d8b2cd978708f
Ren Wang <iamwrm@gmail.com>
2016-11-26 11:41:36 +0800

    member function declaration, definition is afterwards

---
+++ 161126_cpp_try/161126_cpp_try/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdlib>
+
+class student
+{
+private:
+	int sex;
+
+public:void show_sex();  // declaration, definition is afterwards
+	   void change_sex(int sex_input);
+};
+
+void student::show_sex()
+{
+	std::cout << sex<< std::endl;
+}
+
+void student::change_sex(int sex_input)
+{
+	sex = sex_input;
+	//student::sex = sex_input;      there is no difference
+}
+
+int main()
+{
+	student yc;
+	yc.change_sex(1);
+	yc.show_sex();
+
+
+	std::cin.get();
+	return 0;
+}
