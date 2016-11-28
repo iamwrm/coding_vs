82b5ad5439dad79d134b63b9b24c35e46c1dbb6d
Ren Wang <iamwrm@gmail.com>
2016-11-28 12:31:06 +0800

    完成“打出一张牌”，还没有judge

--- 161126_lab8_ex2/161126_lab8_ex2/main.cpp
+++ 161126_lab8_ex2/161126_lab8_ex2/main.cpp
@@ -5,12 +5,30 @@ using namespace std;
 
 int main()
 {
-    player p1;
-    while (1)
-    {
-        p1.ini();
-        p1.show(1);
+	player p1, p2;
+	srand(time(NULL));
 
-        cin.get();
-    }
+	p1.ini();
+	p2.ini();
+
+
+	while (1)
+	{
+		p2.show(1);
+		p1.show(1);
+
+		char p2_input;
+		cin >> p2_input;
+		p2.paly_card(p2_input);
+
+		p2.show(1);
+		p1.show(1);
+
+		char p1_input;
+		cin >> p1_input;
+		p1.paly_card(p1_input);
+	}
+
+	cin.get();
+
 }
--- 161126_lab8_ex2/161126_lab8_ex2/player.h
+++ 161126_lab8_ex2/161126_lab8_ex2/player.h
@@ -12,12 +12,14 @@ class player
     char exist_card[N];
     char last_bolted = '0';
     int score;
+	int change_card(int position, char card);
 
   public:
     void ini();
     void show(int arg);
-    int change_card(int position, char card);
+	int paly_card(char card);
 };
+
 //初始化手牌和场上牌
 void player::ini()
 {
@@ -30,11 +32,10 @@ void player::ini()
     {
         exist_card[i] = '0';
     }
-
-    srand(time(NULL));
+  
     for (int i = 0; i < N; i++)
     {
-        hand[i] = (int)((double)rand() / RAND_MAX * 9) + '1';
+        hand[i] = rand()%9 + '1';
     }
     my_bubblesort(hand, N);
 
@@ -50,10 +51,11 @@ void player::ini()
         }
     }
 }
+
 //arg=1 上帝视角   arg=2 对手视角
 void player::show(int arg)
 {
-    if (arg = 1)
+    if (arg == 1)
     {
         for (int i = 0; i < N; i++)
         {
@@ -105,6 +107,36 @@ int player::change_card(int position, char card)
         cout << "error: there already exists a card";
         return 0;
     }
+}
+
+int player::paly_card(char card_input)
+{
+	int found = 0;
+	int i_hand = 0;
+	for (i_hand=0; i_hand < N; i_hand++)
+	{
+		if (hand[i_hand] == card_input)
+		{
+			found = 1;
+			break;
+		}
+	}
+
+	if (found == 0)
+	{
+		cout << "error: no such card";
+		return 0;// 0 means failed
+	}
+
+	int i_ec = 0;// index of existed card
+	while (exist_card[i_ec] != '0')
+	{
+		i_ec++;
+	}
+	exist_card[i_ec] = card_input;
+	hand[i_hand] = '0';
+
+	return 1;// 1 means succeed
 }
 
 //自定义排序
