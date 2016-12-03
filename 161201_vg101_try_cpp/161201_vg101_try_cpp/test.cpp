9ae7605f55602d65a0fe831f78951bb9560427b1
Ren Wang <iamwrm@gmail.com>
2016-12-03 22:35:28 +0800

    error

--- 161201_vg101_try_cpp/161201_vg101_try_cpp/test.cpp
+++ 161201_vg101_try_cpp/161201_vg101_try_cpp/test.cpp
@@ -5,18 +5,26 @@
 
 using namespace std;
 
+#define INITIAL (0)
+#define EXIT (1)
+#define VALID (2)
+
 
 class database
 {
 public:
+	vector<string> heading;
 	vector<vector<string>> mlist;
 	void add_one_person(vector<string> & input_person);
 	void print_database(ostream &f_cout);
 	void scan_database(fstream& database_file_in);
+	database();
 };
 
 void close_sentence();
 void process_com(string command, int &flag);
+void print_help();
+void show_error(int flag);
 
 int main()
 {
@@ -29,8 +37,8 @@ int main()
 	b1.scan_database(database_file_in);
 
 
-/*
-	vector<string> testv1;
+
+	/*vector<string> testv1;
 	string s1 = "wrewrwerewr", s2 = "Ren 1", s3 = "Dong", s4 = "Shanghai";
 	testv1.push_back(s1);
 	testv1.push_back(s2);
@@ -38,25 +46,25 @@ int main()
 	testv1.push_back(s4);
 	b1.add_one_person(testv1);*/
 
-
-	int call_exit = 0;
-	while (call_exit == 0)
+	cout << "Welcome to my sql database\n"
+		<<"You can enter -h to get help\n";
+	int flag = INITIAL;
+	while (flag != EXIT )
 	{
+		flag = INITIAL;
+
 		cout << "  >";
 
 		string input;
 		cin >> input;
 
-		process_com(input, call_exit);
+		process_com(input, flag);
+
+		show_error(flag);
 	}
-
-
 
 	//close sentence
 	close_sentence();
-
-
-
 
 	//saving into file
 	fstream database_file_out;
@@ -64,7 +72,6 @@ int main()
 	b1.print_database(database_file_out);
 	database_file_out.close();
 
-	cin.get();
 	return 0;
 }
 
@@ -84,7 +91,30 @@ void process_com(string command, int &flag)
 {
 	if (command == "exit")
 	{
-		flag = 1;
+		flag = EXIT;
+		return;
+	}
+	if (command == "-h")
+	{
+		print_help();
+		flag = VALID;
+		return;
+	}
+
+
+}
+
+void print_help()
+{
+	cout <<"You can use the following commands\n"
+		<< "Enter exit to exit\n";
+}
+
+void show_error(int flag)
+{	
+	if (flag == INITIAL)
+	{
+		std::cout << "Invalid input. Try again\n";
 	}
 }
 
@@ -131,4 +161,18 @@ void database::scan_database(fstream & database_file_in)
 		mlist.push_back(one_line_from_file);
 	}
 
+}
+
+database::database()
+{
+	std::string a1, a2, a3, a4;
+	a1 = "LastName";
+	a2 = "FirstName";
+	a3 = "Address";
+	a4 = "City";
+
+	heading.push_back(a1);
+	heading.push_back(a2);
+	heading.push_back(a3);
+	heading.push_back(a4);
 }
