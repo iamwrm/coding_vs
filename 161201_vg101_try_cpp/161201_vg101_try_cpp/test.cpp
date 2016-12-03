51917e6fb6bb50c47113973ea3cf1b43e1ab9ebb
Ren Wang <iamwrm@gmail.com>
2016-12-03 20:51:09 +0800

    database

--- 161201_vg101_try_cpp/161201_vg101_try_cpp/test.cpp
+++ 161201_vg101_try_cpp/161201_vg101_try_cpp/test.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 
-
 class database
 {
 public:
@@ -14,6 +13,73 @@ public:
 	void add_one_person(vector<string> & input_person);
 	void print_database(ostream &f_cout);
 };
+
+void close_sentence();
+void process_com(string command, int &flag);
+
+int main()
+{
+	database b1;
+	vector<string> testv1;
+	string s1 = "Wang", s2 = "Ren 1", s3 = "Dong",s4="Shang hai";
+	testv1.push_back(s1);
+	testv1.push_back(s2);
+	testv1.push_back(s3);
+	testv1.push_back(s4);
+
+
+	b1.add_one_person(testv1);
+
+
+	int call_exit = 0;
+	while (call_exit == 0)
+	{
+		cout << "  >";
+
+		string input;
+		cin >> input;
+
+		process_com(input, call_exit);
+	}
+
+
+
+	//close sentence
+	close_sentence();
+
+	
+
+
+	//saving into file
+	fstream database_file_out;
+	database_file_out.open("testout.txt", std::ostream::out);
+	b1.print_database( database_file_out);
+	database_file_out.close();
+
+	cin.get();
+	return 0;
+}
+
+void close_sentence()
+{
+	cout << "\nThe shell is closing\nSaving your data";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << ".";
+		_sleep(200);
+	}
+	_sleep(1000);
+}
+
+//flag 1:exit
+void process_com(string command,int &flag)
+{
+	if (command == "exit")
+	{
+		flag = 1;
+	} 
+}
+
 
 void database::add_one_person(vector<string> & input_person)
 {
@@ -32,38 +98,3 @@ void database::print_database(ostream &f_cout)
 		f_cout << endl;
 	}
 }
-
-
-int main()
-{
-	database b1;
-	vector<string> testv1;
-	string s1 = "Wang", s2 = "Ren 1", s3 = "Dong",s4="Shang hai";
-	testv1.push_back(s1);
-	testv1.push_back(s2);
-	testv1.push_back(s3);
-	testv1.push_back(s4);
-
-
-	b1.add_one_person(testv1);
-
-
-	cout << "--\n";
-	/*cout << b1.mlist[0][0] << endl;
-	cout << b1.mlist[0][1] << endl;
-	cout << b1.mlist[0][2] << endl;*/
-
-	
-
-
-
-	fstream database_file_out;
-	database_file_out.open("testout.txt", std::ostream::out);
-
-	b1.print_database( database_file_out);
-
-	database_file_out.close();
-	cin.get();
-	return 0;
-}
-
