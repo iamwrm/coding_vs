f6bd5381491c05f84d909d7a767dc5cbdc45547c
Ren Wang <iamwrm@gmail.com>
2016-12-06 23:04:10 +0800

    fileload success

--- 161206_vg101_cpp_lab9/fileload.cpp
+++ 161206_vg101_cpp_lab9/fileload.cpp
@@ -1,15 +1,101 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<vector>
 using namespace std;
 
 
+class point_m
+{
+public:
+	char value;
+	char m1;
+	char m2;
+	point_m()
+	{
+		value = ' ';
+		m1 = ' ';
+		m2 = ' ';
+	}
+};
+
+class store_m
+{
+public:vector<point_m> points;
+	   store_m()
+	   {
+		   for (int i = 0; i < 64; i++)
+		   {
+			   point_m temp1;
+			   points.push_back(temp1);
+		   }
+	   }
+
+
+	   void load_from_file(char* filename)
+	   {
+		   fstream f_cin;
+		   f_cin.open(filename, iostream::in);
+		   string buffer;
+		   points.clear();
+
+		   for (int i = 0; i < 8; i++)
+		   {
+			   getline(f_cin, buffer);
+			   //cout << buffer << endl;
+			   for (int j = 0; j < 8; j++)
+			   {
+				   point_m temp1;
+				   temp1.value = buffer[3 * j];
+				   temp1.m1 = buffer[3 * j + 1];
+				   temp1.m2 = buffer[3 * j + 2];
+
+				   points.push_back(temp1);
+			   }
+		   }
+		   f_cin.close();
+	   }
+
+
+
+	   void save_to_file(char* filename)
+	   {
+		   fstream f_cout;
+		   f_cout.open(filename, iostream::out);
+		   
+		   for (int i = 0; i < 8; i++)
+		   {
+			   for (int j = 0; j < 8; j++)
+			   {
+				   f_cout << points[8 * i + j].value;
+				   f_cout << points[8 * i + j].m1;
+				   f_cout << points[8 * i + j].m2;
+			   }
+
+			   if (i < 7)
+			   {
+				   f_cout << endl;
+			   }
+		   }
+
+
+
+		   f_cout.close();
+	   }
+};
+
 int main()
 {
-fstream f_cin;
-f_cin.open("test.txt",ios::in);
+
+	store_m a1;
+
+	a1.load_from_file("test.txt");
 
 
 
-return 0;
+
+	a1.save_to_file("testout.txt");
+	
+	return 0;
 }
