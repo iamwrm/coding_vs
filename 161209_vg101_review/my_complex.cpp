9ab8fb968e7e450ecd94ca1e343cd556e48b350d
Ren Wang <iamwrm@gmail.com>
2016-12-10 11:45:15 +0800

    实现一元运算符

--- 161209_vg101_review/my_complex.cpp
+++ 161209_vg101_review/my_complex.cpp
@@ -2,58 +2,74 @@
 
 class my_complex
 {
-  public:
-    double real;
-    double imag;
+public:
+	double real;
+	double imag;
 
-  public:
-    my_complex(double real_i = 0, double imag_i = 0)
-    {
-        real = real_i;
-        imag = imag_i;
-    }
+public:
+	my_complex(double real_i = 0, double imag_i = 0)
+	{
+		real = real_i;
+		imag = imag_i;
+	}
 
-    my_complex operator+(const my_complex &b) const
-    {
-        my_complex temp;
-        temp.real = this->real + b.real;
-        temp.imag = this->imag + b.imag;
-        return temp;
-    }
-    void show()
-    {
-        std::cout << real << "+" << imag << std::endl;
-    }
+	my_complex operator+(const my_complex &b) const
+	{
+		my_complex temp;
+		temp.real = this->real + b.real;
+		temp.imag = this->imag + b.imag;
+		return temp;
+	}
+	void show()
+	{
+		std::cout << real << "+" << imag << std::endl;
+	}
 
-    my_complex operator-(const my_complex &b) const
-    {
-        my_complex temp;
-        temp.real = this->real - b.real;
-        temp.imag = this->imag - b.imag;
-        return temp;
-    }
-    my_complex operator*(const my_complex &b) const
-    {
-        my_complex temp;
-        temp.real = this->real * b.real - this->imag * b.imag;
-        temp.imag = this->real * b.imag + this->imag * b.real;
-        return temp;
-    }
+	my_complex operator-(const my_complex &b) const
+	{
+		my_complex temp;
+		temp.real = this->real - b.real;
+		temp.imag = this->imag - b.imag;
+		return temp;
+	}
+	my_complex operator*(const my_complex &b) const
+	{
+		my_complex temp;
+		temp.real = this->real * b.real - this->imag * b.imag;
+		temp.imag = this->real * b.imag + this->imag * b.real;
+		return temp;
+	}
+	my_complex operator-()const
+	{
+		my_complex temp;
+		temp.real = -this->real;
+		temp.imag = -this->imag;
+		return temp;
+	}
 }
 
 ;
 
 int main()
 {
-    my_complex m1(1, 2);
-    my_complex m2(2, 3);
+	my_complex m1(1, 2);
+	my_complex m2(2, 3);
 
-    my_complex m3, m4;
+	my_complex m3, m4;
 
-    m3 = m1 * m2;
-    m4 = m1 - m2;
+	m3 = m1 * m2;
+	m4 = m1 - m2;
 
-    m3.show();
-    m4.show();
-    std::cin.get();
+	m3.show();
+	m4.show();
+	
+
+
+
+	my_complex m5;
+	m5 = -m3;
+	m3.show();
+	m5.show();
+
+	std::cin.get();
 }
